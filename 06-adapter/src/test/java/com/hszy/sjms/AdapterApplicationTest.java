868aa4d3a41e3fa5ab31144a9bfeb4fe97d8317b
fei30_wang <fei30_wang@163.com>
2020-10-08 15:04:32 +0800

    修改

---
+++ 06-adapter/src/test/java/com/hszy/sjms/AdapterApplicationTest.java
@@ -0,0 +1,14 @@
+package com.hszy.sjms;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AdapterApplication.class)
+@WebAppConfiguration
+public class AdapterApplicationTest {
+
+
+}
